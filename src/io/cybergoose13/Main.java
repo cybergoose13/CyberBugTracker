49deddf1454666dcf0cbf1cffcff4e5bce1b8fb0
BuildTools <cybergoose13@gmail.com>
2021-01-25 17:02:56 -0800

    setup connection to sqlite

--- src/io/cybergoose13/Main.java
+++ src/io/cybergoose13/Main.java
@@ -6,18 +6,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        makeConnection();
         Parent root = FXMLLoader.load(getClass().getResource("frame.fxml"));
         primaryStage.setTitle("Pawnee");
         primaryStage.setScene(new Scene(root, 405, 605));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void makeConnection(){
+        String dbName= "test.db";
+        String dbPath= "jdbc:sqlite:";
+        String dbClassName= "org.sqlite.JDBC";
+        Connection connection= null;
+        try{
+            Class.forName(dbClassName);
+            connection= DriverManager.getConnection(dbPath + dbName);
+        }catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Connection established.");
     }
 }
